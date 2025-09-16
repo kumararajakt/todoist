c3ec42a31d284fa1bda971a633bd0d92d6513d2b
Kumar <kumararaja.kt@gmail.com>
2025-09-16 21:08:40 +0530

    changed icon

--- src/systemtraymanager.cpp
+++ src/systemtraymanager.cpp
@@ -151,7 +151,7 @@ void SystemTrayManager::createTrayIcon()
     m_trayIcon = new QSystemTrayIcon(this);
 
     // Set icon - you might want to add a proper icon file
-    QIcon icon = QIcon::fromTheme(QStringLiteral("task-manager"));
+    QIcon icon = QIcon::fromTheme(QStringLiteral("todoist"));
     if (icon.isNull()) {
         // Fallback to a simple colored icon
         QPixmap pixmap(16, 16);
